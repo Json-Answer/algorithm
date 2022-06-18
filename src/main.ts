98234ac9cd71d3308f0486d3642aa2d76fa757eb
Json-Answer <2622336659@qq.com>
2022-06-19 00:28:15 +0800

    更新输出结果显示 update print result

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,16 @@ print("hello word");
 
 const paragraphData = {
   paragraph: "Bob hit a ball, the hit BALL flew far after it was hit.",
-  banned: [""],
+  banned: ["hit"],
 };
 print(worldPop(paragraphData.paragraph, paragraphData.banned));
+// result: ball
 
 print(format(new Date()));
+// result: 2022-05-19 00:26:11
 
 print(unique([{ a: 1 }, { a: 1, b: { a: 1 } }], "a"));
+// result: [ { a: 1, b: { a: 1 } } ]
 
 print(toHex(1200,2))
+// result: 10010110000
